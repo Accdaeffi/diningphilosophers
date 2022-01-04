19e647e8e03783a8f8f8892712e001c0c2546e34
Accdaeffi <noskoff2@yandex.ru>
2022-01-04 18:53:07 +0300

    added comments in Philosophers

--- src/diningphilosophers/Philosopher.java
+++ src/diningphilosophers/Philosopher.java
@@ -2,7 +2,7 @@ package diningphilosophers;
 
 public class Philosopher extends Thread {
 	
-	private static final int EAT_COUNT = 40;
+	private static final int EAT_COUNT = 60;
 	private static final int THINK_TIME = 1000; 
 	private static final int EAT_TIME = 100;
 	
@@ -20,7 +20,11 @@ public class Philosopher extends Thread {
 	@Override
 	public void run() {
 		try {
+			
+			//initial sleep - to start different processes in different time
 			sleep((long) (Math.random()*THINK_TIME));
+			
+			//main cycle - eat, sleep, repeat
 			for (int i=0; i<EAT_COUNT; i++) {
 				wantToEat();
 				sleep((long) (THINK_TIME/2 + Math.random()*THINK_TIME/2));
@@ -32,10 +36,19 @@ public class Philosopher extends Thread {
 	}
 	
 	private void wantToEat() throws InterruptedException {
+		//how many times philosopher will attempt to eat 
 		final int ATTEMPTS_TO_EAT = 100;
+		
+		//true, if philosopher have two forks - and can start eating
 		boolean canEat = false;
 		
+		//philosopher will try to take both forks for ATTEMPTS_TO_EAT times
 		for (int i=0; i<ATTEMPTS_TO_EAT; i++) {
+			
+			/*
+			 * main problem - we should synchronize not by Philosopher.class, but by both forks
+			 * but this is harder
+			*/
 			synchronized(Philosopher.class) {
 				if (leftFork.tryToTake(this) && rightFork.tryToTake(this)) {
 					canEat = true;
@@ -49,12 +62,16 @@ public class Philosopher extends Thread {
 		}
 		
 		if (canEat) {
+			//if we successfully take both forks
 			sleep((long) (Math.random()*EAT_TIME));
 			
 			System.out.println(String.format("Philosopher %d is happy!", this.getPhilId()));
 			
 			leftFork.tryToPut(this);
 			rightFork.tryToPut(this);
+		} else {
+			
+			System.out.println(String.format("Philosopher %d is VERY angry!", this.getPhilId()));
 		}
 	}
 	
